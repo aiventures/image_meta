68d26fdac224821d7f0ec8873ce667b24131b073
aiventures <49552433+aiventures@users.noreply.github.com>
2020-06-15 21:22:47 +0200

    add init file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""module that allows for manipulate exif / image metdata of image files
+   using exiftool. 
+   Features_   
+   - allows for reverse geo encoding (lookup geo coordinates, procide Geo Names),
+     currently uses nominatim / open street map data (caveat: only restricted use allowed!)
+   - Use Tag template to write category / image tags into EXIF / IPTC metadata
+   - Support of Hierarchical Subject metadata / and import of metadata hierarchy   
+"""
